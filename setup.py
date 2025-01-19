d115391ca5907148b12b8748554348e1e360114f
FancyGuitarist <sifer15@ulaval.ca>
2025-01-19 17:56:31 -0500

    fix: added setup.py file for app building

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+APP = ["powermeter_ui.py"]
+
+OPTIONS = {
+    "argv_emulation": False,
+    "packages": ["numpy", "tkinter"],
+    "includes": ["numpy", "tkinter"],
+    "excludes": ["Carbon"],
+    "iconfile": "app_logo.icns",
+    "plist": {
+        "CFBundleName": "PowerMeterApp",
+        "CFBundleDisplayName": "PowerMeterApp",
+        "CFBundleShortVersionString": "0.1.0",
+        "CFBundleVersion": "0.1.0",
+        "CFBundleDevelopmentRegion": "en-CA",
+        "CFBundleExecutable": "PowerMeterApp",
+        "CFBundleIconFile": "app_logo.icns",
+        "NSHumanReadableCopyright": "© 2025 Simon Ferland",
+    },
+}
+
+setup(
+    app=APP,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+    install_requires=["numpy"],
+)
